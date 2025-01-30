04fedc4ab935a373351496e30ab57ed2ff160a45
Julio Meza <juliomeza@gmail.com>
2025-01-30 10:36:27 -0500

    get_or_create_order_lines

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_or_create_orders
+from .views import get_or_create_orders, get_or_create_order_lines
 
 urlpatterns = [
     path('orders/', get_or_create_orders, name="get-orders"),
+    path('order-lines/', get_or_create_order_lines, name="get-order-lines"),
 ]
--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from apps.orders.models import Order
-from .serializers import OrderSerializer
+from apps.orders.models import Order, OrderLine
+from .serializers import OrderSerializer, OrderLineSerializer
 
 @api_view(['GET', 'POST'])
 def get_or_create_orders(request):
@@ -19,5 +19,23 @@ def get_or_create_orders(request):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()  # Guarda la nueva orden en la DB
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST'])
+def get_or_create_order_lines(request):
+    """
+    GET: Retorna la lista de líneas de órdenes.
+    POST: Crea una nueva línea de orden en la base de datos.
+    """
+    if request.method == 'GET':
+        order_lines = OrderLine.objects.all()
+        serializer = OrderLineSerializer(order_lines, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = OrderLineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Guarda la nueva línea de orden en la DB
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
